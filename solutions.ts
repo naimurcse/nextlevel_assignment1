8479376037dc8d8792da35420588962fd24cfbdd
Naimur Rahaman <2015@gmail.com>
2026-05-07 20:36:11 +0600

    Problem 3 has been solved

--- solutions.ts
+++ solutions.ts
@@ -3,3 +3,21 @@ const filterEvenNumbers = (arrayOfNumbers: number[]): number[] => {
   const evenNumbers = arrayOfNumbers.filter((num) => num % 2 == 0);
   return evenNumbers;
 };
+
+// Solution of Problem 2
+const reverseString = (inputString: string): string => {
+  const reversedString = inputString.split("").reverse().join("");
+  return reversedString;
+};
+
+// Solution of Problem 3
+type StringOrNumber = string | number;
+const checkType = (x: StringOrNumber): string => {
+  if (typeof x === "string") {
+    return "String";
+  } else if (typeof x === "number") {
+    return "Number";
+  } else {
+    return "Wrong Input!";
+  }
+};
