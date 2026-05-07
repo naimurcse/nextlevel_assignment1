14e9f3d93931941dee021c44ef6c1911404c6e8d
Naimur Rahaman <2015@gmail.com>
2026-05-07 20:40:35 +0600

    Problem 6 has been solved

--- solutions.ts
+++ solutions.ts
@@ -40,3 +40,28 @@ const toggleReadStatus = (book: Book): Book & { isRead: boolean } => {
     isRead: true,
   };
 };
+
+// Solution of Problem 6
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+  getDetails() {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+const student = new Student("Alice", 20, "A");
+const result = student.getDetails();
